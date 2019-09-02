dde810af6d78ec3cc623c2428f15f52b25419298
mhdrare <mhdfaisalakbar206@gmail.com>
2019-09-02 11:05:57 +0700

    Role

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -19,14 +19,17 @@ namespace BasicWebAPI.Controllers
     public class AuthController : ControllerBase
     {
         private readonly UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
         private readonly SignInManager<ApplicationUser> signInManager;
         private readonly IConfiguration configuration;
         public AuthController(
             UserManager<ApplicationUser> userManager, 
+            RoleManager<IdentityRole> roleManager,
             SignInManager<ApplicationUser> signInManager,
             IConfiguration configuration)
         {
             this.userManager = userManager;
+            this.roleManager = roleManager;
             this.signInManager = signInManager;
             this.configuration = configuration;
         }
@@ -38,7 +41,7 @@ namespace BasicWebAPI.Controllers
             if (result.Succeeded)
             {
                 var user = await userManager.FindByNameAsync(loginParam.UserName);
-                return generateToken(user);
+                return await generateToken(user);
             }
             throw new ApplicationException();
         }
@@ -53,20 +56,43 @@ namespace BasicWebAPI.Controllers
             var result = await userManager.CreateAsync(user, registerParam.Password);
             if (result.Succeeded)
             {
-                await signInManager.SignInAsync(user, false);
-                return generateToken(user);
+                var isRoleExist = await roleManager.RoleExistsAsync("Admin");
+                if(!isRoleExist)
+                {
+                    result = await roleManager.CreateAsync(new IdentityRole 
+                        {
+                            Name = "Admin"
+                        }
+                    );
+                }
+
+                if(!result.Succeeded)
+                    throw new ApplicationException($"Role creation failed, reason: {string.Join(", ", result.Errors.Select(e => e.Description))}");
+
+                result = await userManager.AddToRoleAsync(user, "Admin");
+                if(result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, false);
+                    return await generateToken(user);
+                }
             }
             throw new ApplicationException($"Register failed, reason: {string.Join(", ", result.Errors.Select(e => e.Description))}");
         }
 
-        private object generateToken(ApplicationUser user)
+        private async Task<object> generateToken(ApplicationUser user)
         {
+            var roles = await userManager.GetRolesAsync(user);
             var claims = new List<Claim>
             {
               new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
               new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
               new Claim(ClaimTypes.NameIdentifier, user.Id)
             };
+
+            if(roles.Any())
+            {
+                claims.Add(new Claim(ClaimTypes.Role, roles.FirstOrDefault()));
+            }
             
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtKey"]));
             var expires = DateTime.Now.AddHours(12);
